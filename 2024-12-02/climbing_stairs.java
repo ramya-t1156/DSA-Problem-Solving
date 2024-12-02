4d858af19e6253444bebebd038cbd6dd3f6daa6c
ramya-t1156 <153516045+ramya-t1156@users.noreply.github.com>
2024-12-02 20:36:28 +0530

    Create climbing_stairs.java

---
+++ 2024-12-02/climbing_stairs.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int climbStairs(int n) {
+        if(n<=1) return 1;
+        int prev2 = 1;
+        int prev = 1;
+        for(int i = 2 ; i<=n ; i++){
+            int current = prev + prev2;
+            prev2 = prev;
+            prev = current;
+        }
+        return prev;
+    }
+}
