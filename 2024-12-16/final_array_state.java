7c5822935dec823859521ff9e5b4e2e8d37165c0
ramya-t1156 <153516045+ramya-t1156@users.noreply.github.com>
2024-12-16 18:42:38 +0530

    3264. Final Array State After K Multiplication Operations I

---
+++ 2024-12-16/final_array_state.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int findMin(int[] nums){
+        int min = 0;
+        for(int i = 1;i<nums.length;i++){
+            if(nums[i]<nums[min]){
+                min = i;
+            }
+        }
+        return min;
+    }
+    public int[] getFinalState(int[] nums, int k, int multiplier) {
+        
+        for(int i = 0;i<k;i++){
+            int idx = findMin(nums);
+            nums[idx] = nums[idx]*multiplier;
+        }
+        return nums;
+    }
+}
