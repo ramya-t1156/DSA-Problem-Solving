2fff18a0a0afef666cd0404e6d5cb474ceeb6bdc
ramya-t1156 <153516045+ramya-t1156@users.noreply.github.com>
2025-01-16 19:17:48 +0530

    2425. Bitwise XOR of All Pairings - Problem Solution

---
+++ 2025-01-16/bitwise_xor_of_all_pairings.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int xorAllNums(int[] nums1, int[] nums2) {
+        int res = 0;
+        if(nums1.length%2==1){
+            for(int num:nums2){
+                res ^= num;
+            }
+        }
+        if(nums2.length%2==1){
+            for(int num:nums1){
+                res ^= num;
+            }
+        }
+        return res;
+    }
+}
