4252138d380429d8e9a718b69f89623a80647afd
ramya-t1156 <153516045+ramya-t1156@users.noreply.github.com>
2025-02-11 22:50:15 +0530

    461. Hamming Distance - Problem Solution

--- 2025-02-11/hamming_distance.java
+++ 2025-02-11/hamming_distance.java
@@ -1,7 +1,7 @@
 // Time Complexity - O(n logn)
 class Solution {
     public int hammingDistance(int x, int y) {
-        int xorVal = x^y;
+        int xorVal = x^y; 
         int count = 0;
         while(xorVal>0){
             if((xorVal & 1)>0){
